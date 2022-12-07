a93fc3eb97a2f8caac233028b615f23212adbed2
Burak İskender <burak.iskendr@gmail.com>
2022-12-07 21:34:33 +0300

    :sparkles: add netlify functions

---
+++ functions/search.js
@@ -0,0 +1,26 @@
+const axios = require("axios");
+
+exports.handler = async function (event, context) {
+  try {
+    const { q } = event.queryStringParameters;
+    const URL = `${process.env.WEATHER_APP_BASE_URL}/search.json?key=${process.env.WEATHER_APP_API_KEY}&q=${q}`;
+    const response = await axios
+      .get(URL, {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          "Accept-Encoding": "identity",
+        },
+      })
+      .then((res) => res.data);
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response),
+    };
+  } catch (error) {
+    return {
+      statusCode: 404,
+      body: error.toString(),
+    };
+  }
+};
