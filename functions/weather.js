44c694efc1f9c4fac6cc330130e5aec7e34804be
Burak İskender <burak.iskendr@gmail.com>
2022-12-12 23:28:58 +0300

    :recycle: refactored code

--- functions/weather.js
+++ functions/weather.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 exports.handler = async function (event, context) {
   try {
     const { q } = event.queryStringParameters;
-    const URL = `${process.env.WEATHER_APP_BASE_URL}/forecast.json?key=${process.env.WEATHER_APP_API_KEY}&q=${q}&days=7&aqi=no&alerts=no`;
+    const URL = `${process.env.WEATHER_APP_BASE_URL}/forecast.json?key=${process.env.WEATHER_APP_API_KEY}&q=${q}&days=7&aqi=yes&alerts=no`;
     const response = await axios
       .get(URL, {
         method: "GET",
